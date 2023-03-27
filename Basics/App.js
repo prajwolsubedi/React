5179037d73addcbbf582fcf506a6b77518e0bb52
prajwolsubedi <prajwolsubedizzz@gmail.com>
2023-03-27 21:49:11 +0545

    update

--- Basics/App.js
+++ Basics/App.js
@@ -1,51 +1,30 @@
 //React element is just a plain javascript object
 
-/* 
-    Parcel does all of this 
-    HMR- hot module replacement
-    File watcher algorithm - C++
-    Building 
-    MINIFY
-    Cleaning our Code
-    Dev and Production build
-    Super Fast build algorithm 
-    Image optimization
-    Caching while development
-    Compression - rewriting code
-    Compatible with older versions of browser
-    HTTPS on dev
-
-    */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-// const heading1 = React.createElement("h1", {} , "Heading1")
-// const heading2 = React.createElement("h1", null , "Heading2")
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h1",
-  },
-  "Hell"
+//using createElement()
+const container = React.createElement(
+  "div",
+  {},
+  React.createElement("ul", {}, [
+    React.createElement("li", {key:"1"}, "I"),
+    React.createElement("li", {key:"2"}, "Love"),
+    React.createElement("li", {key:"3"}, "Elon Musk"),
+  ])
 );
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title2",
-    key: "h2",
-  },
-  "Hello"
+//using jsx
+const container2 = (
+  <div>
+    <ul>
+      <li key={1}>I</li>
+      <li key={2}>Love</li>
+      <li key={3}>Elon Musk</li>
+    </ul>
+  </div>
 );
+console.log(container);
 
-console.log(heading2);
-
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
-//Root is the place where react runs
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
